9d46054cb221d23f57aa0ec6e57ead2b950238d3
Alex Angelini <alex.louis.angelini@gmail.com>
2015-11-02 22:07:12 -0500

    use linenoise

--- alisp/src/core.rs
+++ alisp/src/core.rs
@@ -1,23 +1,18 @@
 #![allow(dead_code)]
 
-use std::io;
-use std::io::prelude::*;
+extern crate linenoise;
 
 pub fn read_line(prompt: &str) -> Option<String> {
-    print!("{}", prompt);
-    io::stdout().flush().ok().expect("Could not flush stdout");
-
-    let mut line = String::new();
-    io::stdin().read_line(&mut line)
-        .ok()
-        .expect("Failed to read line");
-
-    let len = line.len();
-    line.remove(len - 1);
-
-    if line == "exit" {
-        None
-    } else {
-        Some(line)
+    let val = linenoise::input(prompt);
+    match val {
+        None => { None }
+        Some(input) => {
+            linenoise::history_add(&input);
+            if input == "exit" {
+                None
+            } else {
+                Some(input)
+            }
+        }
     }
 }
